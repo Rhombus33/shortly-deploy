cf9b434e2c5e55da3d6f091e168eef48153133e1
Hack Reactor Students <accounts@hackreactor.com>
2016-02-10 21:18:11 -0800

    start monogo db refactor

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,29 @@
-var db = require('../config');
-var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+// var db = require('../config');
+// var bcrypt = require('bcrypt-nodejs');
+// var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
-});
+// var User = db.Model.extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   initialize: function() {
+//     this.on('creating', this.hashPassword);
+//   },
+//   comparePassword: function(attemptedPassword, callback) {
+//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+//       callback(isMatch);
+//     });
+//   },
+//   hashPassword: function() {
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
+// });
 
-module.exports = User;
+// var mongoose = require('mongoose');
+// var schema = require('../config');
+
+// var User = mongoose.model('User', schema.userSchema);
+// module.exports = User;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var app = require('./server-config.js');
 
 var port = 3000;
+var mongoose = require('mongoose');
+
+var dbPort = 'mongodb://localhost/27017';
+mongoose.connect(dbPort);
 
 app.listen(port);
 
